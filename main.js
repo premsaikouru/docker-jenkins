93afbab5b8ed99e7c2a2b647081ae11df353391f
premsaikouru <78961826+premsaikouru@users.noreply.github.com>
2021-02-20 14:11:56 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-var http =require('http');
-
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {'content-Type': 'text/plain'});
-  response.end('Hello World\n');
-});
-
-server.listen(8000);
-console.log('server listening at http://127.0.0.1:8000/');
